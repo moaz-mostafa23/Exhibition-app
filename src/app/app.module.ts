f4714b2781e5f257fcac751e75b400df9fefc35f
Adnan <adnanm2002@outlook.com>
2023-12-08 19:28:59 +0300

    Connected to Firebase + added CRUD service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import {provideFirestore, getFirestore} from '@angular/fire/firestore'
+import {provideAnalytics, getAnalytics} from '@angular/fire/analytics'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import {provideAuth, getAuth} from '@angular/fire/auth'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+const firebaseConfig = { apiKey: "AIzaSyCpXrqctZrI4oee53qBKtZyWJBmIA7JDn4",
+authDomain: "exhibition-app-eebdc.firebaseapp.com",
+projectId: "exhibition-app-eebdc",
+storageBucket: "exhibition-app-eebdc.appspot.com",
+messagingSenderId: "983679106549",
+appId: "1:983679106549:web:29fda1cd732e4bb8a1f4bc",
+measurementId: "G-HKJM4K919E"};
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot({mode: 'ios'}), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot({mode:'ios'}), AppRoutingModule, HttpClientModule, provideFirebaseApp(()=> initializeApp(firebaseConfig)), provideFirestore(()=> getFirestore()), provideAnalytics(()=> getAnalytics()), provideAuth(()=> getAuth())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/crud.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, doc, addDoc, getDoc, updateDoc, deleteDoc, getDocs, DocumentReference } from '@angular/fire/firestore';
+import { DocumentData } from 'firebase/firestore';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+  constructor(private firestore: Firestore) { }
+
+  // Create a new document in a collection
+  async createDocument(collectionName: string, data: any): Promise<DocumentReference<DocumentData> | any> {
+    try {
+      const collectionRef = collection(this.firestore, collectionName);
+      return await addDoc(collectionRef, data);
+    } catch (error) {
+      console.error("Error creating document: ", error);
+    }
+  }
+
+  // Get all documents from a collection as an Observable
+  getDocuments(collectionName: string): Observable<DocumentData[]> {
+    try {
+      const collectionRef = collection(this.firestore, collectionName);
+      return collectionData(collectionRef, { idField: 'id' }); // Optionally specify an ID field
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+      return of([]);
+    }
+  }
+
+  // Get a specific document by its ID
+  async getDocumentById(collectionName: string, documentId: string): Promise<DocumentData | any> {
+    try {
+      const documentRef = doc(this.firestore, `${collectionName}/${documentId}`);
+      const docSnapshot = await getDoc(documentRef);
+      if (docSnapshot.exists()) {
+        return docSnapshot.data() as DocumentData;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error("Error getting document: ", error);
+    }
+  }
+
+  // Update a document by its ID
+  async updateDocument(collectionName: string, documentId: string, data: any): Promise<void> {
+    try {
+      const documentRef = doc(this.firestore, `${collectionName}/${documentId}`);
+      await updateDoc(documentRef, data);
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+  }
+
+  // Delete a document by its ID
+  async deleteDocument(collectionName: string, documentId: string): Promise<void> {
+    try {
+      const documentRef = doc(this.firestore, `${collectionName}/${documentId}`);
+      await deleteDoc(documentRef);
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  }
+
+  // Delete all documents in a collection
+  async deleteCollection(collectionName: string): Promise<void> {
+    try {
+      const collectionRef = collection(this.firestore, collectionName);
+      const snapshot = await getDocs(collectionRef);
+      snapshot.forEach((doc) => {
+        deleteDoc(doc.ref);
+      });
+    } catch (error) {
+      console.error("Error deleting collection: ", error);
+    }
+  }
+}
