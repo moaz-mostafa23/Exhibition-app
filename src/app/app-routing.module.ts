6c1cb6c067fda46532de75d046b821ba56084ab2
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-12-18 12:45:32 +0300

    View Event Page: Initial commit #6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
   },  {
     path: 'edit-profile',
     loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
+  },
+  {
+    path: 'event',
+    loadChildren: () => import('./event/event.module').then( m => m.EventPageModule)
   }
 
 
