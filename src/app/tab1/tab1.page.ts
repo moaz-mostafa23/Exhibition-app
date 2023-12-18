d1af715a959070676caca15c72078eaf69e8ae53
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-12-18 14:45:31 +0300

    Added Halls Retrieval for client + Search functionality
    Only remains forseeing the time of each hall and storing that
    inside the interface, not the database.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,7 +4,7 @@ import { User } from '../auth.service';
 import {Auth, getAuth} from '@angular/fire/auth';
 import { AuthService } from '../auth.service';
 import { CrudService } from '../crud.service';
-import { Event } from '../crud.service';
+import { Event, Hall } from '../crud.service';
 import { UtilityService } from '../utility.service';
 
 @Component({
@@ -18,11 +18,15 @@ export class Tab1Page implements OnInit, OnDestroy {
   private auth: Auth | any;
   userType: string = '';
 
-  events : Event[] = {} as Event[]; // this list will be updated and maintained along with the data
+  events : Event[] = [] as Event[]; // this list will be updated and maintained along with the data
   // we will get from the observable. This will help us when filtering search results of the user.
-  eventsCopy : Event[] = {} as Event[]; // this list will be used to store the original data from the observable.
+  eventsCopy : Event[] = [] as Event[]; // this list will be used to store the original data from the observable.
   
+  halls : Hall[] = {} as Hall[];
+  hallsCopy : Hall[] = {} as Hall[];
+
   
+
   constructor(
     private authService: AuthService,
     private loadingController: LoadingController,
@@ -32,7 +36,7 @@ export class Tab1Page implements OnInit, OnDestroy {
     public utilityService : UtilityService,
     public navController : NavController,
   ) {
-    
+
 
   }
 
@@ -48,6 +52,13 @@ export class Tab1Page implements OnInit, OnDestroy {
           this.user = await this.authService.getUserData();
           loading.dismiss();
           this.userType = this.user.userType;
+
+                 // loading user's home page data
+         if(this.userType == 'client')
+            await this.getHalls();
+         else if (this.userType == 'attendee')
+            await this.getEvents();
+
         }else{
           loading.dismiss();
           this.navCtrl.navigateForward('/login');
@@ -58,7 +69,9 @@ export class Tab1Page implements OnInit, OnDestroy {
       }
     });
 
-    await this.getEvents();
+
+
+
   }
 
 
@@ -90,12 +103,51 @@ export class Tab1Page implements OnInit, OnDestroy {
         });
 
         console.log(this.events);
+        console.log(this.eventsCopy);
         loading.dismiss();
       });
     }catch(err){
       let alert = await this.alertController.create({
         header: 'Error',
         message: 'There was an error loading the events. Please try again later.',
+        buttons: ['OK']
+      });
+      loading.dismiss();
+      alert.present();
+      console.log(err);
+    }
+  }
+
+  async getHalls(){
+    const loading = await this.loadingController.create({
+      message: 'Loading Halls Information...',
+    });
+    await loading.present();
+
+    try{
+      this.crudService.getDocuments('halls').subscribe((halls)=>{
+        this.halls = halls as Hall[];
+        this.hallsCopy = halls as Hall[];
+      
+        // this.halls.forEach(element => {
+        //   element.start_date = this.utilityService.convertFirebaseTimestamp(element.start_date);
+        //   element.end_date = this.utilityService.convertFirebaseTimestamp(element.end_date); 
+        // });
+
+        // // same for the eventsCopy
+        // this.hallsCopy.forEach(element => {
+        //   element.start_date = this.utilityService.convertFirebaseTimestamp(element.start_date);
+        //   element.end_date = this.utilityService.convertFirebaseTimestamp(element.end_date); 
+        // });
+
+        console.log("Halls:")
+        console.log(this.halls);
+        loading.dismiss();
+      });
+    }catch(err){
+      let alert = await this.alertController.create({
+        header: 'Error',
+        message: 'There was an error loading the halls. Please try again later.',
         buttons: ['OK']
       });
       loading.dismiss();
@@ -114,6 +166,20 @@ export class Tab1Page implements OnInit, OnDestroy {
     if(searchTerm && searchTerm.trim() != ''){
       this.eventsCopy = this.eventsCopy.filter((event)=>{
         return (event.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+      });
+    }
+  }
+
+  searchHalls(hall: any){
+    const searchTerm = hall.target.value;
+    if(searchTerm == ''){
+      this.hallsCopy = this.halls;
+      return;
+    }
+    
+    if(searchTerm && searchTerm.trim() != ''){
+      this.hallsCopy = this.hallsCopy.filter((hall)=>{
+        return (hall.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
       });
     }
   }
