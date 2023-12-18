5647783a5f2118d1776a8ac13fb49cc77f7e9fc8
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-12-18 12:47:52 +0300

    Added functions for Converting From/To Base64
    the IDs of events/halls are passed through
    the url after they are encoded using Base64

--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -7,4 +7,14 @@ export class UtilityService {
 
   constructor() { }
 
+  textToBase64(text: string): string {
+    return btoa(unescape(encodeURIComponent(text)));
+  }
+  
+  base64ToText(base64: string): string {
+    return decodeURIComponent(escape(atob(base64)));
+  }
+
+
+
 }
