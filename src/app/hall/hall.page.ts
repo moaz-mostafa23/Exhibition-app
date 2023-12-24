c90f44ea53132bf33f8ff1780aed579cdd2e617a
Moaz Mostafa <m.mostafa@calo.app>
2023-12-24 20:02:59 +0300

    filter by date

--- src/app/hall/hall.page.ts
+++ src/app/hall/hall.page.ts
@@ -5,44 +5,48 @@ import { NavController } from '@ionic/angular';
 import { UtilityService } from '../utility.service';
 import { Location } from '@angular/common';
 
-
 @Component({
   selector: 'app-hall',
   templateUrl: './hall.page.html',
   styleUrls: ['./hall.page.scss'],
-  styles: [`
-    ion-content {
-      background-color: #f0f0f0;
-    }
+  styles: [
+    `
+      ion-content {
+        background-color: #f0f0f0;
+      }
 
-    h2 {
-      text-align: center;
-    }
+      h2 {
+        text-align: center;
+      }
 
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      margin-top: 20px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
+      table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      }
 
-    th, td {
-      padding: 12px;
-      text-align: left;
-      border: 1px solid #ddd;
-    }
+      th,
+      td {
+        padding: 12px;
+        text-align: left;
+        border: 1px solid #ddd;
+      }
 
-    th {
-      background-color: #4CAF50;
-      color: white;
-    }
-  `],
+      th {
+        background-color: #4caf50;
+        color: white;
+      }
+    `,
+  ],
 })
 export class HallPage implements OnInit {
   hall: Hall = {} as Hall;
   hallName: string = '';
   hallId: string | null = '';
   events: any[] = [];
+  showTable = false;
+  selectedOption: string = 'last7days'; // Default option is last 7 days
 
   constructor(
     public activatedRoute: ActivatedRoute,
@@ -50,7 +54,7 @@ export class HallPage implements OnInit {
     public crudService: CrudService,
     public utilityService: UtilityService,
     private router: Router,
-    private location: Location,
+    private location: Location
   ) { }
 
   async ngOnInit() {
@@ -91,7 +95,7 @@ export class HallPage implements OnInit {
   async getReservationHistory(hallId: string) {
     try {
       const reservationHistory = await this.crudService.getDocumentsByQuery('events', 'hall_id', hallId);
-      console.log('Reservation history:', reservationHistory)
+      console.log('Reservation history:', reservationHistory);
       return reservationHistory;
     } catch (error) {
       console.error('Error getting reservation history:', error);
@@ -112,27 +116,55 @@ export class HallPage implements OnInit {
   async loadReservationHistory() {
     try {
       const reservationHistory = await this.getReservationHistory(this.hallId as string);
-      this.events = reservationHistory as Event[];
-      // Format timestamps to human-readable dates
-      this.events.forEach(event => {
-        event.start_date = this.utilityService.convertFirebaseTimestamp(event.start_date);
-        event.end_date = this.utilityService.convertFirebaseTimestamp(event.end_date);
+
+      // Filter reservation history based on the selected option
+      const currentDate = new Date();
+      const startDateFilter = new Date();
+
+      if (this.selectedOption === 'last7days') {
+        startDateFilter.setDate(currentDate.getDate() - 7);
+      } else if (this.selectedOption === 'last30days') {
+        startDateFilter.setDate(currentDate.getDate() - 30);
+      }
+
+      this.events = reservationHistory.filter((event) => {
+        // Check if 'start_date' property exists and is not undefined
+        if (event && event['start_date']) {
+          const eventStartDate = new Date(this.utilityService.convertTimestamp(event['start_date']));
+          const dateDifference: number = (currentDate.getTime() - eventStartDate.getTime()) / (1000 * 60 * 60 * 24);
+
+          // Check if the event is within the selected time range
+          return dateDifference <= (this.selectedOption === 'last7days' ? 7 : 30);
+        }
+        return false; // If 'start_date' is undefined, exclude the event
       });
+
+      // Format timestamps to human-readable dates
+      this.events.forEach((event) => {
+        event['start_date'] = this.utilityService.convertFirebaseTimestamp(event['start_date']);
+        event['end_date'] = this.utilityService.convertFirebaseTimestamp(event['end_date']);
+      });
+
       console.log('Reservation history:', this.events);
     } catch (error) {
       console.error('Error loading reservation history:', error);
     }
   }
-  showTable = false;
+
+
+
+
+
 
   toggleTable(): void {
     this.showTable = !this.showTable;
+    // Reload reservation history when toggling the table
+    if (this.showTable) {
+      this.loadReservationHistory();
+    }
   }
 
   isTableShown(): boolean {
     return this.showTable;
   }
-
-
-
 }
