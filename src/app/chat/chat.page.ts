c19953a948144167724e806737f4dcdc976e6df1
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-12-24 18:37:42 +0300

    Handled fail in sending chat msgs by admin to client in chat page

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CrudService } from '../crud.service';
 import { AuthService } from '../auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat',
@@ -12,7 +13,11 @@ export class ChatPage implements OnInit {
 
   receiverId : any = '';
   msg = '';
-  constructor(private activatedRoute : ActivatedRoute, private crudService : CrudService, private authService : AuthService,) { }
+  constructor(private activatedRoute : ActivatedRoute, 
+              private crudService : CrudService, 
+              private authService : AuthService,
+              private alertController : AlertController,
+              ) { }
 
   ngOnInit() {
     this.receiverId = this.activatedRoute.snapshot.paramMap.get('receiverId');
@@ -20,7 +25,17 @@ export class ChatPage implements OnInit {
 
   sendMessage(){
     let currentUserId = this.authService.getCurrentUser()?.uid;
-    this.crudService.sendMessage(currentUserId, this.receiverId, this.msg);
+    if(
+      !this.crudService.sendMessage(currentUserId, this.receiverId, this.msg);
+      ){
+        console.log("Error sending message");
+        let alert = await this.alertController.create({
+          header: 'Error',
+          message: 'Error sending message, please try again later!',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
     this.msg = '';
   }
 
